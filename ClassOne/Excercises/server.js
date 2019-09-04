cf46c16088bdd8a8db39501d1929fc4564e4311e
stefafrontani <stefafrontani@gmail.com>
2019-09-04 20:55:40 -0300

    Excercise 6 - Import profile file

--- ClassOne/Excercises/server.js
+++ ClassOne/Excercises/server.js
@@ -5,6 +5,7 @@ const port = 8080;
 
 const home     = require('./home');
 const users    = require('./users');
+const profile  = require('./profile');
 
 const server = http.createServer((req, res) => {
   res.setHeader("Content-Type", "text/html");
