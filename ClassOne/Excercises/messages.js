6c90c4b3f01c4d33660535c4f9b38a4a436955ac
stefafrontani <stefafrontani@gmail.com>
2019-09-04 20:53:20 -0300

    Excersise 5 - Create messages.js file

---
+++ ClassOne/Excercises/messages.js
@@ -0,0 +1,13 @@
+const messages = `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title> MESSAGES </title>
+      </head>
+      <body>
+        <h1> MESSAGES </h1>
+      </body>
+    </html>
+`
+
+module.exports = messages;
