ee59714899e7b56e220bf6c82a1c1d0eca161bdf
stefafrontani <stefafrontani@gmail.com>
2019-09-04 20:22:13 -0300

    [class_one_excersise_three] - Create template.js file with basic html structure

---
+++ ClassOne/Excercises/template.js
@@ -0,0 +1,11 @@
+const template = `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title>My first html response </title>
+      </head>
+      <body>
+        <h1> H1 HEADING </h1>
+      </body>
+    </html>
+`
