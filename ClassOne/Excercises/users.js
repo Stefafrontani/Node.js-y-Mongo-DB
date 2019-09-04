524c63bf001f875635076bd1dcf4848cc775c1f3
stefafrontani <stefafrontani@gmail.com>
2019-09-04 20:53:14 -0300

    Excersise 5 - Create users.js file

---
+++ ClassOne/Excercises/users.js
@@ -0,0 +1,13 @@
+const users = `
+  <!DOCTYPE html>
+    <html>
+      <head>
+        <title> USERS </title>
+      </head>
+      <body>
+        <h1> USERS </h1>
+      </body>
+    </html>
+`
+
+module.exports = users;
