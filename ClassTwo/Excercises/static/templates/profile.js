0e5e39adbfa82f2f8396595a72ecf980d7865ec5
stefafrontani <stefafrontani@gmail.com>
2019-09-09 15:46:49 -0300

    [class_two_exercises] - 3 - Create profile html as a template strings

---
+++ ClassTwo/Excercises/static/templates/profile.js
@@ -0,0 +1,15 @@
+const profile = `
+    <!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <meta http-equiv="X-UA-Compatible" content="ie=edge">
+            <title> PROFILE </title>
+        </head>
+        <body>
+            <h1> PROFILE </h1>
+        </body>
+    </html>
+`
+module.exports = profile;
