de87556e8957c23535fe73d11e0e3102c3e7f7e8
stefafrontani <stefafrontani@gmail.com>
2019-09-16 21:39:05 -0300

    [class_two_notes] Initialize express server with handlebars configuration

---
+++ ClassThree/LectureNotes/E1-HANDLEBARS/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+const motorhandlebars = exphbs.create({
+    extname: "hbs",
+});
+
+app.engine("hbs", motorhandlebars.engine);
+app.set("view engine", "hbs");
+
+app.listen(3000,() => {
+    console.log("Servidor listo");
+});
