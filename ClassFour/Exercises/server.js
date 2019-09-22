a5a8271dc6a9090c31f4e0ef8546f0917d7bbde4
stefafrontani <stefafrontani@gmail.com>
2019-09-22 00:07:05 -0300

    [class_four_exercises] - 1 - Initialize express server with handlebars as a views engine

---
+++ ClassFour/Exercises/server.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const exphbs = require("express-handlebars");
+const bodyParser = require("body-parser");
+
+app.use(express.static("content"));
+
+// Configure post requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Configure express to use handlebars
+// First: create handlebars engine
+const motor = exphbs.create(
+    {
+        defaultLayout: 'main',
+        extname: 'hbs'
+    });
+
+// Configure express to use handlebars engine
+app.engine('hbs', motor.engine);
+// Tell express view's engines to use hbs files
+app.set("view engine", "hbs");
+
+const port = 3000;
+let server = app.listen(port, () => {
+    console.log(`Starting server on port: ${port}`);
+});
