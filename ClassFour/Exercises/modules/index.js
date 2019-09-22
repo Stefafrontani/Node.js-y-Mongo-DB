14d6c60dae383df974207ef8ea3a0c7d88f7c5f1
stefafrontani <stefafrontani@gmail.com>
2019-09-22 02:26:53 -0300

    [class_four_exercises] - 3 - Create users collections

--- ClassFour/Exercises/modules/index.js
+++ ClassFour/Exercises/modules/index.js
@@ -1,12 +1,17 @@
 const mongoDriver = require('mongodb');
 const mongoClient = mongoDriver.MongoClient;
 
+let users;
+
 mongoClient.connect('mongodb://localhost:27017', (err, client) => {
     if (err) {
         console.log('Could not connect to mongodb');
     } else {
         let db = client.db("ClassFourExercises");
+        users = db.collection('users')
     }
 });
 
-module.exports = db;
+module.exports = {
+  users: {},
+}
